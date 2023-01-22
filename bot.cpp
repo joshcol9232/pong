8a12b03451323621598849a8306c3fb8d9243e2e
Josh Colclough <colcloughj2@cardiff.ac.uk>
2023-01-22 20:23:06 +0000

    remove paddle jitter

--- bot.cpp
+++ bot.cpp
@@ -18,13 +18,13 @@ void Bot::update(const double dt, Paddle* target_paddle) const {
   if(current_target_y_ < y) {
     // Move up
     if (y - dy < current_target_y_) {  // Precise movement
-      dt_rel = dy / target_paddle->get_speed();
+      dt_rel = (current_target_y_ - y) / target_paddle->get_speed();
     }
     target_paddle->move(dt_rel, false);
   } else {
     // Move down
     if (y + dy > current_target_y_) {  // Precise movement
-      dt_rel = dy / target_paddle->get_speed();
+      dt_rel = (current_target_y_ - y) / target_paddle->get_speed();
     }
     target_paddle->move(dt_rel, true);
   }
@@ -44,22 +44,8 @@ double Bot::solve(const double x_pos,
             << "\tball_dir_y=" << ball_direction.y() << std::endl
             << "\ttarget_x=" << target_x << std::endl;
 
-
-  // First find y intercept & gradient
-  // y = mx + c
-  // m = dy/dx
-//  if (ball_direction.x() < 1e-20) {  // ~ 0
-//    return
-//  }
-
   const double m = ball_direction.y() / ball_direction.x();
   const double c = y_pos - m * x_pos;
-
-//  if (std::abs(c) > 1e20) {  // Some arbitrary huge number
-//    std::cout << "Bot::solve: C IS MASSIVE" << std::endl;
-//    // Never crosses y axis, therefore parallel
-//    return y_pos;
-//  }
 
   std::cout << "Bot::solve: m=" << m << "\tc=" << c << std::endl;
 
@@ -88,29 +74,26 @@ double Bot::solve(const double x_pos,
                  radius, ball_direction, target_x);
   }
 
-  std::cout << "Bot::solve finished." << std::endl;
+  std::cout << "Bot::solve finished.\t\r" << std::flush;
+
   return y_result;
 }
 
 void Bot::change_target(const Ball& b, const bool lefts_turn) {
   const auto& ball_direction = b.get_direction();
 
+  const double leading_paddle_edge = constants::PADDLE_OFFSET +
+                                     constants::PADDLE_WIDTH/2;
+
   if (lefts_turn) {
     // Find intersection with left wall
     // x = 0
-    current_target_y_ = solve(b.get_x(),
-                              b.get_y(),
-                              b.get_radius(),
-                              ball_direction,
-                              constants::PADDLE_OFFSET + constants::PADDLE_WIDTH/2);
+    current_target_y_ = solve(b.get_x(), b.get_y(), b.get_radius(),
+                              ball_direction, leading_paddle_edge);
   } else {    // moving right
     // right wall
     // x = WINDOW_WIDTH
-    current_target_y_ = solve(b.get_x(),
-                              b.get_y(),
-                              b.get_radius(),
-                              ball_direction,
-                              constants::WINDOW_WIDTH -
-                              (constants::PADDLE_OFFSET + constants::PADDLE_WIDTH/2));
+    current_target_y_ = solve(b.get_x(), b.get_y(), b.get_radius(), ball_direction,
+                              constants::WINDOW_WIDTH - leading_paddle_edge);
   }
 }
